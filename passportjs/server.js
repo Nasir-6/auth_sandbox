f774b31850150e5005ed64df065435d179699b2a
Mohammed Nasir <mohammed.nasir@ntegra.co.uk>
2024-02-28 19:26:31 +0000

    fix express server not working

--- passportjs/server.js
+++ passportjs/server.js
@@ -64,3 +64,8 @@ app.post("/login", (req, res) => {
 
   res.send(`Authed as ${user.name}`);
 });
+
+app.listen(3000, function (err) {
+  if (err) console.log("Error in server setup");
+  console.log("Server listening on Port", 3000);
+});
